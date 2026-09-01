38786f7ecd146bec63a356132d367adc08464957
workramsah <workramsah@gmail.com>
2026-09-01 14:38:39 +0545

    added some kind page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Page(){
     return(
         <div>
-            <div>dashboard</div>
+            <div>dashboard page</div>
             <Link href="/terms-and-conditions">Terms and Conditions</Link>
             <Link href="/privacy-policy">Privacy Policy</Link>
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
-  return (
-    <div>
-      <div>
-        hello world from the main page
-      </div>
-    </div>
-  );
+export default function Page(){
+    return(
+        <div>
+            <div>home page</div>
+            <Link href="/terms-and-conditions">Terms and Conditions</Link>
+            <Link href="/privacy-policy">Privacy Policy</Link>
+        </div>
+    )
 }
