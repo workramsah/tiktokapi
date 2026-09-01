b66a915ed471ad86bdef2d9291df6950a0670be1
workramsah <workramsah@gmail.com>
2026-09-01 14:29:06 +0545

    add name of link in main page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function Page(){
     return(
         <div>
             <div>dashboard</div>
+            <Link href="/terms-and-conditions">Terms and Conditions</Link>
+            <Link href="/privacy-policy">Privacy Policy</Link>
         </div>
     )
 }
