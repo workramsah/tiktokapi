460562b9fd78b205d7c923f459c1562e3373bb97
workramsah <workramsah@gmail.com>
2026-09-02 17:36:02 +0545

    fix 2

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,19 +4,18 @@ import Link from "next/link";
 import { useRef } from "react";
 
 export default function Page(){
-    const inputRef = useRef(null);
-    console.log(inputRef)
+    const inputRef = useRef<HTMLInputElement>(null);
 
-        function handleInputChange() {
-            inputRef.current.focus()
-        }
+    function handleInputChange() {
+        inputRef.current?.focus()
+    }
     return(
         <div>
             <div>dashboard page</div>
             <button onClick={handleInputChange}>Copy</button>
             <Link href="/terms-and-conditions">Terms and Conditions</Link>
             <Link href="/privacy-policy">Privacy Policy</Link>
-            <input placeholder="Enter your text here..."ref={inputRef}></input>
+            <input placeholder="Enter your text here..." ref={inputRef}></input>
         </div>
     )
 }
